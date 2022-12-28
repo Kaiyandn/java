35082fc98a4575873e608c7660e4162186d1e506
Kaiyandn <95992379+Kaiyandn@users.noreply.github.com>
2022-12-28 17:32:22 +1100

    Create Arithmetic_Operators.java

---
+++ Arithmetic_Operators.java
@@ -0,0 +1,36 @@
+int a = 4;
+int b = 5;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+
+//adds to var together 
+c = a + b;
+System.out.println(c);
+
+//subtracts two var together
+d = c - a;
+System.out.println(d);
+
+//multiplies two var together
+e = c * b;
+System.out.println(e);
+
+//divides two var together
+f = e / b;
+System.out.println(f);
+
+//returns the division remainder
+g = f % e;
+System.out.println(g);
+
+//Increments the value by one
+h = a++;
+System.out.println(h);
+
+//Decrements the value by one
+i = --b;
