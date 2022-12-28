7a4bede7c8edd2ba90fa532c6083f001fa822a57
Kaiyandn <95992379+Kaiyandn@users.noreply.github.com>
2022-12-28 17:29:58 +1100

    Update Data _Types.java

--- Data _Types.java
+++ Data _Types.java
@@ -1,12 +1,7 @@
-# *Data Type Templates*
-
-### Data Types
-
-```java
+//Data Types
 int myNum = 5;               // Integer (whole number)
 double myDoubleNum = 3.14;   // Double (Decimal or non decimal number)
 float myFloatNum = 5.99f;    // Floating point number
 char myLetter = 'D';         // Character
 boolean myBool = true;       // Boolean
 String myText = "Hello";     // String
-```
